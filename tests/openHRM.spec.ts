39a6527e5d31461ad6782cd54b31e1357815f3ca
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-09-14 00:19:44 +0545

    Testing login page

--- tests/openHRM.spec.ts
+++ tests/openHRM.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Open HRM Login page',async()=>{
-    test('The login page loads',async()=>{
-        
+test.describe('Open HRM Login page', () => {
+    test('The login page loads', async ({ page }) => {
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        await expect(page.locator("//h5[text()='Login']")).toBeVisible()
+        await expect(page.locator("button[type='submit']")).toBeVisible()
     })
 })
