5daba2798e6453b478960a52c5ebdb6854cbf777
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-09-14 19:22:25 +0545

    Testing login

--- tests/openHRM.spec.ts
+++ tests/openHRM.spec.ts
@@ -1,9 +1,24 @@
 import { test, expect } from '@playwright/test';
+import { testData } from '../utils/test-data';
+
 
 test.describe('Open HRM Login page', () => {
-    test('The login page loads', async ({ page }) => {
+    test.beforeEach(async ({ page }) => {
         await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    });
+    test('The login page loads', async ({ page }) => {
         await expect(page.locator("//h5[text()='Login']")).toBeVisible()
         await expect(page.locator("button[type='submit']")).toBeVisible()
+    })
+    test('should successfully log in user with valid credentials', async ({ page }) => {
+        const usernameField = page.getByPlaceholder("Username")
+        const passwordField = page.getByPlaceholder("Password")
+        await usernameField.fill(testData.validUser.username)
+        await passwordField.fill(testData.validUser.password)
+        await page.getByRole('button').filter({ hasText: 'Login' }).click();
+        //after login
+        await expect(page.locator("//h6[text()='Dashboard']")).toBeVisible()
+        await expect(page.locator("//a[contains(@href,'viewAdminModule')]")).toBeVisible()
+
     })
 })
